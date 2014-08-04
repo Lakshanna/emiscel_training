ca346e20fb4941897b0aef9903ade9d04e271b23
tnemiscel <tnemiscel@gmail.com>
2014-08-04 10:51:39 +0530

    Student Project

---
+++ siva/sivasite/sivasite/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+from sivaapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from sivaapp.views import UserView
+from sivaapp.views import *
+
+urlpatterns = patterns('',
+	url(r'^$', 'sivaapp.views.loginauth', name='loginmain'),
+	url(r'^add/$', UserView.as_view()),
+	url(r'^list/$', 'sivaapp.views.list', name='list'),
+	url(r'^signup/', 'sivaapp.views.signup', name='signup'),
+	url(r'^home/', 'sivaapp.views.home', name='home'),
+	url(r'^loginout/', 'sivaapp.views.loginout', name='loginout'),
+
+	#url(r'^delete/', 'sivaapp.views.delete', name='delete'),
+
+	url(r'^edit/(?P<pk>\d+?)/update/$',UpdateView.as_view(),name='update'),
+	url(r'^delete/(?P<pk>\d+?)/$',new_delete.as_view(),name='delete'),
+
+	url(r'^admin/', include(admin.site.urls)),
+)
